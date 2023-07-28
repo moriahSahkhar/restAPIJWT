44cecc218fca69563406257642fcd68c229cb93c
moriahSahkhar <moriah.s1234@gmail.com>
2023-07-28 14:07:43 +0530

    Important Addition

--- src/main/java/com/restAPISecurity/restAPI/config/SecurityConfig.java
+++ src/main/java/com/restAPISecurity/restAPI/config/SecurityConfig.java
@@ -2,23 +2,31 @@ package com.restAPISecurity.restAPI.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+	
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	private AuthenticationProvider authenticationProvider;
 	
 	@Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity
-        .cors().and()
 		.csrf().disable()
+        .cors().and()
                 .authorizeHttpRequests(
                         (authorize) -> authorize
                                 .requestMatchers("/api/test/**").permitAll()
@@ -27,10 +35,13 @@ public class SecurityConfig {
                 .sessionManagement(
                         (session) -> session
                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .exceptionHandling()
-                .accessDeniedPage("/access-denied");
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                
 
         return httpSecurity.build();
     }
+
+
 
 }
