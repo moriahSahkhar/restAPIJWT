79a4d0db1661a595c8c19c217d0adf49cac9c2bf
moriahSahkhar <moriah.s1234@gmail.com>
2023-07-28 14:17:26 +0530

    Update SecurityConfig.java

--- src/main/java/com/restAPISecurity/restAPI/config/SecurityConfig.java
+++ src/main/java/com/restAPISecurity/restAPI/config/SecurityConfig.java
@@ -1,32 +1,31 @@
 package com.restAPISecurity.restAPI.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import lombok.RequiredArgsConstructor;
-
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 	
+	@Autowired
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	
+	@Autowired
 	private AuthenticationProvider authenticationProvider;
 	
 	@Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity
-		.csrf().disable()
-        .cors().and()
+				.csrf()
+				.disable()
                 .authorizeHttpRequests(
                         (authorize) -> authorize
                                 .requestMatchers("/api/test/**").permitAll()
